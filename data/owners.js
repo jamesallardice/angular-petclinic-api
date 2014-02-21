6f5a9c92610b4d0cd9ba1545acdf819609da39db
James Allardice <james.allardice@capita.co.uk>
2014-02-21 15:47:58 +0000

    Owner data and route

---
+++ data/owners.js
@@ -0,0 +1,57 @@
+module.exports = [
+    {
+        id: 1,
+        firstName: "Thomas",
+        lastName: "Smith"
+    },
+    {
+        id: 2,
+        firstName: "Bill",
+        lastName: "Long"
+    },
+    {
+        id: 3,
+        firstName: "Adam",
+        lastName: "Jones"
+    },
+    {
+        id: 4,
+        firstName: "Emily",
+        lastName: "Miller"
+    },
+    {
+        id: 5,
+        firstName: "Heather",
+        lastName: "Foster"
+    },
+    {
+        id: 6,
+        firstName: "Kenneth",
+        lastName: "James"
+    },
+    {
+        id: 7,
+        firstName: "Dennis",
+        lastName: "Gray"
+    },
+    {
+        id: 8,
+        firstName: "Russell",
+        lastName: "Anderson"
+    },
+    {
+        id: 9,
+        firstName: "Lisa",
+        lastName: "Smith"
+    },
+    {
+        id: 10,
+        firstName: "Adam",
+        lastName: "Foster"
+    },
+    {
+        id: 11,
+        firstName: "Bobby",
+        lastName: "Price"
+    }
+];
